73d37224db9dbf7bde8d8137e2d04f2fc375ce57
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-08-31 02:11:26 +0400

    Bilingual adj

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,19 +1,22 @@
+import { useTranslation } from 'react-i18next';
 import { Button } from '../ui/button';
 
 export default function Header() {
+  const { t } = useTranslation();
+
   return <header className='bg-primary text-primary-foreground flex items-center h-[70px] w-full'>
     <img src="/assets/logo-sm.svg" alt="Logo" className='px-4 py-3' />
     <aside className="flex px-4 py-1 ml-auto gap-4">
       <Button variant="ghost" className='p-0'>
         <div className='flex flex-col gap-[2px] items-center'>
           <img src="/assets/icons/search.svg" alt="Search" className='w-6 h-6' />
-          ძიება
+          {t('search')}
         </div>
       </Button>
       <Button variant="ghost" className='p-0'>
         <div className='flex flex-col gap-[2px] items-center'>
           <img src="/assets/icons/profile.svg" alt="Profile" className='w-6 h-6' />
-          შესვლა
+          {t('login')}
         </div>
       </Button>
     </aside>
--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import { json, type LinksFunction, type LoaderArgs } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,10 +6,24 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { ThemeProvider } from "./components/theme-provider";
+import { useTranslation } from 'react-i18next';
+import { parseCookies } from 'server/utils/request';
+
+export async function loader({ request }: LoaderArgs) {
+  const cookies = parseCookies(request.headers.get('Cookie') ?? '');
+  let locale = cookies.lang || 'ka';
+  
+  return json({ locale });
+}
+
+export let handle = {
+  i18n: "common",
+};
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -17,8 +31,11 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App() {
+  let { locale } = useLoaderData<typeof loader>();
+  let { i18n } = useTranslation();
+
   return (
-    <html lang="en" className='dark'>
+    <html lang={locale} className='dark' dir={i18n.dir()}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
