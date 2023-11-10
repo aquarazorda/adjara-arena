850eab7b1350d7e39e52a7bc5e9c6b42c414c5a5
lukaurushadze <urushadze.luka@student.ehu.lt>
2023-11-11 00:21:57 +0400

    AKB-405 fb registration

---
+++ app/routes/_main.fb-register/fbRegistrationForm.tsx
@@ -0,0 +1,88 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSubmit } from '@remix-run/react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import type { z } from 'zod';
+import { Button } from '~/components/ui/button';
+import { Form, FormControl, FormField, FormItem } from '~/components/ui/form';
+import { Input } from '~/components/ui/input';
+import { registrationSchema } from '~/lib/schemas/register';
+
+export default function FbRegistrationForm() {
+  const { t } = useTranslation();
+  const form = useForm<z.infer<typeof registrationSchema>>({
+    resolver: zodResolver(registrationSchema),
+  });
+
+  const [codeSent, setCodeSent] = useState(false);
+
+  const submit = useSubmit();
+
+  return (
+    <Form {...form}>
+      <FormField
+        control={form.control}
+        name="phoneNumber"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input placeholder={t('phone_number')} {...field} readonly={codeSent && 'readonly'} />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      {codeSent && (
+        <div className="flex flex-col gap-3">
+          <FormField
+            control={form.control}
+            name="verificationCode"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder={t('verification_code')} {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-between">
+            <p className="text-xs text-silver-ground dark:text-silver/60 cursor-pointer">{t('არ მიგიღია კოდი?')}</p>
+            <p className="text-xs text-silver-ground dark:text-silver/60 cursor-pointer">
+              <span className="dark:text-white-secondary text-dark-silver-text mr-2">00:10</span>
+              {t('გააგზავნე ხელახლა')}
+            </p>
+          </div>
+        </div>
+      )}
+      <div className="flex flex-col gap-4">
+        <FormField
+          control={form.control}
+          name="termsAndConditions"
+          render={({ field }) => (
+            <FormItem>
+              <Input
+                placeholder={t('termsAndConditions')}
+                {...field}
+                type="checkbox"
+                onChange={(e: any) => {
+                  const formData = new FormData();
+                  formData.append('value', e.target.checked);
+                  formData.append('user_agent', window.navigator.userAgent);
+
+                  submit(formData, {
+                    method: 'post',
+                  });
+
+                  field.onChange(e);
+                }}
+              />
+            </FormItem>
+          )}
+        />
+        <Button variant="success" size="lg" onClick={() => setCodeSent(true)}>
+          <p className="text-base font-regular_uppercase">{t('რეგისტრაცია')}</p>
+        </Button>
+      </div>
+    </Form>
+  );
+}
--- app/routes/_main.register/route.tsx
+++ app/routes/_main.register/route.tsx
@@ -4,45 +4,39 @@ import Separator from '~/components/ui/separator';
 import RegistrationForm from './registrationForm';
 import type { ActionFunction } from '@remix-run/node';
 import { acceptedTerms } from 'server/db/schema/accepted_terms';
-import { db } from 'server/db'
+import { db } from 'server/db';
+import RegistrationLayout from '~/components/registration-layout';
 
 type NewAcceptedTerms = typeof acceptedTerms.$inferInsert;
 
 const newTerms = (data: NewAcceptedTerms) => {
   return db.insert(acceptedTerms).values(data).returning();
-}
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const ip = request.headers.get('X-Client-IP') || '';
   const formData = await request.formData();
-  const value = formData.get('value')
-  const user_agent = formData.get('user_agent')
+  const value = formData.get('value');
+  const user_agent = formData.get('user_agent');
 
-  return newTerms({ value: value === 'true', event_type: 'onChange', user_agent: String(user_agent), ip })
-}
+  return newTerms({ value: value === 'true', event_type: 'onChange', user_agent: String(user_agent), ip });
+};
 
 export default function RegistrationPage() {
   const { t } = useTranslation();
 
   return (
-    <div className="dark:bg-dark-grey bg-white flex min-h-[calc(100vh-70px)] md:min-h-[calc(100vh-80px)] dark:text-white text-grey">
-      <div className="flex-1 hidden md:block">
-        <img className="w-full h-full object-cover" src="" alt="adjarabetarena" />
-      </div>
-      <div className="flex-1 flex justify-center items-center py-[16px]">
-        <div className="flex flex-col md:max-w-[460px] w-full gap-6 p-4">
-          <p className="font-medium text-lg">{t('registration')}</p>
-          <Button
-            variant="ghost"
-            size="lg"
-            className="flex w-full items-center bg-blue text-base text-white hover:bg-blue/80 lowercase"
-          >
-            {t('register_facebook')}
-          </Button>
-          <Separator />
-          <RegistrationForm />
-        </div>
-      </div>
-    </div>
+    <RegistrationLayout className="gap-6 p-4">
+      <p className="font-medium text-lg">{t('registration')}</p>
+      <Button
+        variant="ghost"
+        size="lg"
+        className="flex w-full items-center bg-blue text-base text-white hover:bg-blue/80 lowercase"
+      >
+        {t('register_facebook')}
+      </Button>
+      <Separator />
+      <RegistrationForm />
+    </RegistrationLayout>
   );
 }
