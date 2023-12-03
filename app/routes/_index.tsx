d8d3324af91be82db43b8016f8bbd84144b217b8
lukaurushadze <urushadze.luka@student.ehu.lt>
2023-12-04 03:12:40 +0400

    Setanta Box timers

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,7 +22,60 @@ export default function Index() {
           <p className="mt-[3px] leading-[22px] text-">უყურე უფასოდ</p>
         </a>
         <div>
-          <SetantaBox date="2023-11-30 02:45:00" clock url="/register" />
+          <SetantaBox
+            // date="2023-11-04 02:00:00"
+            live
+            url="/register"
+            img="https://adjarabetarena.com/s3-static/Luton---Arsenal.png"
+          />
+          <SetantaBox
+            date={
+              new Date(
+                new Date().getFullYear(),
+                new Date().getMonth(),
+                new Date().getDate(),
+                new Date().getHours() + 1,
+                0,
+                0
+              )
+            }
+            url="/register"
+            img="https://adjarabetarena.com/s3-static/Luton---Arsenal.png"
+          />
+          <SetantaBox
+            date={
+              new Date(
+                new Date().getFullYear(),
+                new Date().getMonth(),
+                new Date().getDate() + 1,
+                new Date().getHours(),
+                0,
+                0
+              )
+            }
+            url="/register"
+            img="https://adjarabetarena.com/s3-static/Luton---Arsenal.png"
+          />
+          <SetantaBox
+            date={
+              new Date(
+                new Date().getFullYear(),
+                new Date().getMonth(),
+                new Date().getDate() + 2,
+                new Date().getHours(),
+                0,
+                0
+              )
+            }
+            url="/register"
+            img="https://adjarabetarena.com/s3-static/Luton---Arsenal.png"
+          />
+          <SetantaBox
+            date="Mon Dec 04 2024 03:10:00 GMT+0400 (Georgia Standard Time)"
+            url="/register"
+            img="https://adjarabetarena.com/s3-static/Luton---Arsenal.png"
+          />
+          <SetantaBox ended url="/register" img="https://adjarabetarena.com/s3-static/Luton---Arsenal.png" />
         </div>
       </div>
     </Container>
