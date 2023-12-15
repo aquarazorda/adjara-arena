5213c842342e0be30ed2da144b090ea7d748746a
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-12-15 16:34:32 +0400

    chore: removed images from icons

--- app/components/icons/SportIcon/index.tsx
+++ app/components/icons/SportIcon/index.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from 'react';
-
 type SportName = 'sport' | 'football' | 'basketball' | 'tennis' | 'ufc' | 'rugby' | 'other';
 
 const SportIcon = ({ name, size = 22 }: { name: string | SportName; size?: number }) => {
-  const [imgSrc, setImgSrc] = useState('');
-
-  useEffect(() => {
-    import(`./${name}.png`).then((module) => setImgSrc(module.default)).catch(() => setImgSrc('./sport.png'));
-  }, [name]);
-
-  return <img src={imgSrc} alt={name} width={size} height={size} />;
+  return <img src={`/icons/${name}.png`} alt={name} width={size} height={size} />;
 };
 
 export default SportIcon;
